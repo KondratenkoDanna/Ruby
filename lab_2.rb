1d086ad441f0b2d19052709fdfcb806d6543ff9a
Kondratenko Danna <danna.kondratenko.00@mail.ru>
2022-02-13 17:20:00 +0300

    2.2

--- lab_2.rb
+++ lab_2.rb
@@ -1,27 +1,55 @@
-# puts "Cумма цифр = #{ARGV[0].to_i.digits.sum}"
+# # puts "Cумма цифр = #{ARGV[0].to_i.digits.sum}"
 
 num = ARGV[0]
 
-def sum_digit(numb)
-  numb.to_i.digits.sum
+# def sum_digit(numb)
+#   numb.to_i.digits.sum
+# end
+
+# def min_digit(numb)
+#   numb.to_i.digits.max
+# end
+
+# def max_digit(numb)
+#   numb.to_i.digits.min
+# end
+
+# def pr_digit(numb)
+#   pr = 1
+#   list = numb.to_i.digits
+#   list.each { |value| pr *= value }
+#   pr
+# end
+
+# puts "Cумма цифр = #{sum_digit(num)}"
+# puts "Мин. цифра = #{min_digit(num)}"
+# puts "Макс. цифра = #{max_digit(num)}"
+# puts "Произведение цифр = #{pr_digit(num)}"
+
+# Метод 1
+
+# Метод 2
+def sum_3(num)
+  sum = 0
+  for val in num.digits
+    if (val % 3).zero?
+      sum += val
+    end
+  end
+  sum
 end
 
-def min_digit(numb)
-  numb.to_i.digits.max
-end
+puts sum_3(ARGV[0].to_i)
 
-def max_digit(numb)
-  numb.to_i.digits.min
-end
+# Метод 3
 
-def pr_digit(numb)
-  pr = 1
-  list = numb.to_i.digits
-  list.each { |value| pr *= value }
-  pr
-end
 
-puts "Cумма цифр = #{sum_digit(num)}"
-puts "Мин. цифра = #{min_digit(num)}"
-puts "Макс. цифра = #{max_digit(num)}"
-puts "Произведение цифр = #{pr_digit(num)}"
+
+
+
+
+
+
+
+
+
