fd400f32775b910936e1ebeaa67b6933ac9a7eea
Kondratenko Danna <danna.kondratenko.00@mail.ru>
2022-03-06 12:18:45 +0300

    5.2 date

--- lab_4.rb
+++ lab_4.rb
@@ -1,6 +1,6 @@
 # Задание 4
 # 2
-str = ARGV[0].to_s
+str = ARGV[0]
 def order_symb(str, size, ind = 0, flag = true)
   if size - 1 == ind
     return flag
@@ -19,13 +19,9 @@ end
 # puts order_symbol(str)
 
 #17
-def name_f(str)
-  /\/[^\/]+\./.match(str)
-end
-
-def name_file(str)
- name_f(str)[0][1..-2]
-end
+# def name_file(str)
+#   /\/[^\/]+\./.match(str)[0][1..-2]
+# end
 
 # 10
 def kol_symb(str, ind = 0, list = [], kol = 0)
@@ -46,12 +42,39 @@ def kol_A(str)
   str.size - /[^A]+/.match(str)[0].size
 end
 
-p kol_A(str)
 # 17
 
+
 # Cортировка 2 4 8 10
+def search_date(str)
+  l = []
+  reg = /[0-3]?[0-9]\s(января|февраля|марта|апреля|мая|июня|июля|августа|сентября|октября|ноября|декабря)\s[1-2]+[0-9]{3}/
+   while str.match(reg).to_s != ""
+     l << str.match(reg).to_s
+     str = str.sub(str.match(reg).to_s, "")
+   end
+   l
+end
 
+puts search_date(str)
 
-
-
+func = [
+    kol_A(str),
+    kol_symb(str),
+    # name_file(str),
+    order_symbol(str)
+]
+def user_selection()
+  puts "Введите действие: 1 - найти индексы двух наименьших элементов массива
+                          2 - найти количество задействованных символов латиницы в этой строке
+                          3 - найти имя файла без расширения
+                          4 - проверить упорядочены ли строчные символы этой строки по возрастанию
+                          5 - найти все даты, которые описаны в виде 31 февраля 2007"
+  func_choice = gets.chomp
+  if func_choice >= 1 || func_choice <=5
+    eval(func_choice[func - 1])
+  else
+    puts "Неllo world"
+  end
+end
 
