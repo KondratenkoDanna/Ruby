b26cc4cd32e1f72a05bc1273f6721ab51078b52c
Kondratenko Danna <danna.kondratenko.00@mail.ru>
2022-03-23 10:35:29 +0300

    1

--- task_1.rb
+++ task_1.rb
@@ -1,20 +1,71 @@
 class Departament
-  attr_accessor :name, :number_phone
+  attr_accessor :name, :highligth_duty
 
-  def initialize(name, number_phone)
-    @name = name
-    @number_phone = number_phone
+  def number_phone
+    @number_phone
   end
 
-  def print
-    puts "Имя: #{self.name}\nНомер телефона: #{self.number_phone}"
+  def number_phone=(val)
+    unless Departament.check_phone?(val)
+      puts 'Номер введен неправильно! Попробуйте еще раз: '
+      val = gets.chomp
+    end
+    @number_phone = val
+  end
+
+  def initialize(name, number_phone, duties = [])
+    @name, @duties = name, duties
+    self.number_phone = (number_phone)
+  end
+
+  def pr_duties
+    return @duties
+  end
+
+  def pr_highligth_duty
+    return @duties[@highligth_duty]
+  end
+
+  def add_duties(val)
+    @duties << val
+  end
+
+  def del_duties(ind)
+    @duties.delete_at(ind)
+  end
+
+  def highligth(ind)
+    return @highligth_duty = ind
+  end
+
+  def replace_highligth_duty(text)
+    return @duties[@highligth_duty] = text
+  end
+
+  def to_s
+    unless @duties.empty?
+      duty = ''
+      @duties.each { |el| duty += el + ', '}
+      return "Имя: #{@name}\nНомер телефона: #{@number_phone}\nОбязанности: #{duty[0..-3]}."
+    end
+    return "Имя: #{@name}\nНомер телефона: #{@number_phone}"
+  end
+
+  def Departament.check_phone?(number_phone)
+    puts number_phone.class
+    /\+?[1-9][0-9]{10}/ === number_phone.to_s
   end
 end
 
-us1 = Departament.new('Ivan', 46553)
-us2 = Departament.new('Nikolai', 01456)
-us3 = Departament.new('Pol', 45921)
-us1.print
-us2.print
-us3.print
-# puts s.name, s.number_phone
+puts Departament.new('Ivan', +79008908899, ['спать', 'лежать'])
+# us1.name
+# us1.duty = 1
+# us1.add_duties('кушать')
+# puts us1.pr_duties
+# us1.add_duties('отдыхать')
+# p us1.highligth(1)
+# puts us1.pr_duties
+# puts Departament.check_phone?
+# puts us1.pr_highligth_duty(0)
+# puts us1.pr_duties
+
