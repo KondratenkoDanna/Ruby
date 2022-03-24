1f3c3291682cee7ea2dd41ecafb5dff702a30407
Kondratenko Danna <danna.kondratenko.00@mail.ru>
2022-03-24 16:47:50 +0300

    2

--- task_1.rb
+++ task_1.rb
@@ -1,9 +1,8 @@
 class Departament
   attr_accessor :name, :highligth_duty
+  @@list_objects = []
 
-  def number_phone
-    @number_phone
-  end
+  attr_reader :number_phone
 
   def number_phone=(val)
     unless Departament.check_phone?(val)
@@ -16,10 +15,19 @@ class Departament
   def initialize(name, number_phone, duties = [])
     @name, @duties = name, duties
     self.number_phone = (number_phone)
+    @@list_objects << self
+  end
+
+  def Departament.all
+    @@list_objects
   end
 
   def pr_duties
-    return @duties
+    puts @duties.join(', ')
+  end
+
+  def pr_duties_in_str
+    return @duties.join(', ')
   end
 
   def pr_highligth_duty
@@ -44,28 +52,107 @@ class Departament
 
   def to_s
     unless @duties.empty?
-      duty = ''
-      @duties.each { |el| duty += el + ', '}
-      return "Имя: #{@name}\nНомер телефона: #{@number_phone}\nОбязанности: #{duty[0..-3]}."
+      duty = @duties.join(', ')
+      return "Имя: #{@name}\nНомер телефона: #{@number_phone}\nОбязанности: #{duty}."
     end
     return "Имя: #{@name}\nНомер телефона: #{@number_phone}"
   end
 
   def Departament.check_phone?(number_phone)
-    puts number_phone.class
-    /\+?[1-9][0-9]{10}/ === number_phone.to_s
+    /\+?[1-9][0-9]{10}/ === number_phone
   end
+
+  def Departament.read_from_text(name_file)
+    list_obj = []
+    file_data = File.open(name_file, 'r') {|f| f.read}
+    list_obj_without_separators = file_data.split("#")
+    list_obj_without_separators.shift
+    for obj in list_obj_without_separators
+      obj = obj.split("\n")
+      obj.shift
+      obj[2] = obj[2].split(", ")
+      list_obj << obj
+    end
+    list_obj
+  end
+
+  def Departament.print
+    for obj in @@list_objects
+      p obj[2].empty?
+      if obj[2].empty?
+        puts "Имя: #{obj[0]}\nНомер телефона: #{obj[1]}"
+      else
+        puts "Имя: #{obj[0]}\nНомер телефона: #{obj[1]}\nОбязанности: #{obj[2].join(', ')}."
+      end
+    end
+  end
+
+  def Departament.write_to_txt(name_file)
+    name_file += '.txt'
+    str = ""
+    for obj in @@list_objects
+      str += "\n#\n#{obj.name}\n#{obj.number_phone}"
+      unless obj.pr_duties_in_str.empty?
+        str += "\n#{obj.pr_duties_in_str}"
+      end
+    end
+    str += "\n#\n"
+    file_data = File.open(name_file, 'w') {|f| f.write str[1..]}
+  end
+
+  def Departament.write_to_YAML(name_file)
+    name_file += '.yaml'
+    require 'yaml'
+    file = File.open( name_file, 'w' )
+      YAML.dump( @@list_objects, file )
+    file.close
+  end
+
+  def Departament.read_from_YAML(name_file)
+    name_file += '.yaml'
+    require 'yaml'
+      YAML.load_file(name_file)
+  end
+
 end
 
-puts Departament.new('Ivan', +79008908899, ['спать', 'лежать'])
-# us1.name
-# us1.duty = 1
-# us1.add_duties('кушать')
-# puts us1.pr_duties
-# us1.add_duties('отдыхать')
-# p us1.highligth(1)
-# puts us1.pr_duties
-# puts Departament.check_phone?
-# puts us1.pr_highligth_duty(0)
-# puts us1.pr_duties
+# class.ancestors - вывод предков
+s = Departament.new('Ivan', '+79008908899', ['спать', 'лежать'])
+s1 = Departament.new('Vova', '+79008908800', ['питаться', 'кушать'])
+s1.add_duties('spat')
+s1.name = 'Alex'
+s1.number_phone = '89000007007878'
+puts Departament.all
+# puts s1
+# p Departament.read_from_text('input.txt')
+# Departament.print
+# puts Departament.read_from_YAML('t_y')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
